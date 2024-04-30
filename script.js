502664c71a4af166941d4455218e513744f73d4d
nsporter17 <149802430+nsporter17@users.noreply.github.com>
2024-04-30 14:34:16 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//input variables
+let inputs = document.querySelector(".inputs");
+let math = document.querySelector(".math-grade");
+let english = document.querySelector(".english-grade");
+let history = document.querySelector(".history-grade");
+let science = document.querySelector(".science-grade");
+let add = document.querySelector(".add");
+
+function convert(inputs){
+if(inputs === "A" || inputs === "a"){
+    return 4;
+}
+
+else if(inputs === "B" || inputs === "a"){
+    return 3;
+}
+else if(inputs === "C" || inputs === "c"){
+        return 2;
+}
+else if(inputs === "D" || inputs === "d"){
+    return 1;
+}
+else if(inputs === "F" || inputs === "f"){
+    return 0;
+}
+
+
+
+
+
+}
+
+add.onclick = function(){
+
+
+
+
+
+
+
+
+
+    
+};
+
